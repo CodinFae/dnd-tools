619caedebdcd59d1d81dbef1c72560d52487607e
Samuel Gillieron <MY_NAME@example.com>
2026-04-01 06:54:27 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<nav>
+	<a href="/">home</a>
+	<a href="/initiative-tracker">initiative tracker</a>
+</nav>
+{@render children()}
---
+++ src/routes/initiative-tracker/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Initiative Tracker</h1>
+
+<p>Start by adding some characters to the tracker, and then click start.</p>
