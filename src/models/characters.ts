aac950e5d7608adf358df175a418eb802e6a0e7e
Samuel Gillieron <MY_NAME@example.com>
2026-04-02 07:03:17 +0200

    Add a way to add the different characters

---
+++ src/models/characters.ts
@@ -0,0 +1,6 @@
+export interface character {
+    name : string,
+    initiativeModifier : number,
+    hp : number,
+    
+}
--- src/routes/initiative-tracker/+page.svelte
+++ src/routes/initiative-tracker/+page.svelte
@@ -1,3 +1,38 @@
-<h1 class="text-4xl">Initiative Tracker</h1>
+<script lang="ts">
+	import type { character } from "../../models/characters";
 
-<p>Start by adding some characters to the tracker, and then click start.</p>
+    let characters = $state<character[]>([])
+
+    const removeCharacter = (name : string) => characters = characters.filter(a => a.name != name)
+    const addCharacter = (event : SubmitEvent & {currentTarget: HTMLFormElement}) => {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget);
+        console.log(formData)
+        characters.push({
+            name: formData.get("name"),
+            hp: formData.get("hp"),
+            initiativeModifier: formData.get("initiative")
+        } satisfies character)
+    }
+</script>
+
+<h1 class="text-4xl">Initiative Tracker</h1>
+<ul class="list">
+    {#each characters as actor }
+        <li class="list-row">{actor.name} - Initiative: {actor.initiativeModifier} HP: {actor.hp}</li>
+        {:else}
+            <li class="list-row">No actors added.</li>
+        {/each}
+    <li class="list-row">
+        <form onsubmit={addCharacter} class="flex flex-row">
+            <input type="text" class="input" placeholder="Name" name="name">
+            <input type="number" class="input" placeholder="Initiative modifier" name="initiative">
+            <input type="number" class="input" placeholder="HP" name="hp">
+            <button class="btn" type="submit">Submit</button>
+        </form>
+    </li>
+</ul>
+<div class="join">
+  <button class="btn join-item">Add</button>
+  <button class="btn join-item btn-accent">Start</button>
+</div>
