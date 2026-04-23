a78fc6245e642e11654b54d5c7e487f4665fca1b
Samuel Gillieron <MY_NAME@example.com>
2026-04-23 07:08:51 +0200

    Allow to edit a party

--- src/routes/party/+page.svelte
+++ src/routes/party/+page.svelte
@@ -1,13 +1,51 @@
-<script>
-    import {partyStore} from "$lib/stores/party.store.svelte"
+<script lang=ts>
+    import {partyStore, removeCharacter} from "$lib/stores/party.store.svelte"
 	import { feetToSquare } from "$lib/utils/dnd.utils";
 	import { formatModifier, formatAttributeModifier } from "$lib/utils/format.utils";
+
+    const sortedParty = $derived(partyStore.toSorted((a,b) => a.name.localeCompare(b.name)))
+    
+    let selectedNames = $state(new Set<string>());
+    const selected = $derived(
+    sortedParty.filter(p => selectedNames.has(p.name))
+);
+
+
+	const toggleSelectedList = (name : string ) => {
+        return () => {
+            if (selectedNames.has(name)) {
+                selectedNames.delete(name);
+            } else {
+                selectedNames.add(name);
+            }
+            selectedNames = new Set(selectedNames);
+        }
+    }
+
+
+    let indeterminateAll = $derived(selectedNames.size > 0 && selectedNames.size < sortedParty.length)
+    let checkedAll = $derived(selectedNames.size >= sortedParty.length)
+
+    const removeSelected = () => {
+        selectedNames.forEach(name => {
+            removeCharacter(name)
+            selectedNames.delete(name)
+        });
+    }
+	
 </script>
 
 <div class="overflow-auto">
+    <div class="flex flex-row align-middle justify-items-center">
+        <p>{selected.length} selected</p>
+        <button class="btn btn-ghost btn-error" disabled={selected.length <1} onclick={removeSelected}>Delete</button>
+    </div>
     <table class="table">
         <thead>
             <tr>
+                <th>
+                    <input type="checkbox" class="checkbox" indeterminate="{indeterminateAll}" checked={checkedAll}/>
+                </th>
                 <th>
                     Name
                 </th>
@@ -33,8 +71,13 @@
         </thead>
         <tbody>
 
-        {#each partyStore.toSorted((a,b) => a.name.localeCompare(b.name)) as partyMember}
+        {#each sortedParty as partyMember}
         <tr>
+            <td>
+                <input type="checkbox" class="checkbox" 
+                checked={selectedNames.has(partyMember.name)}
+                onclick={toggleSelectedList(partyMember.name)}>
+            </td>
             <td>
                 {partyMember.name}
             </td>
